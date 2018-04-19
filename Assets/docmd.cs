5c92be70c3ea7c0cb60239366f7c6d6470767984
410517980 <s1051798@gm.pu.edu.tw>
2018-04-20 01:58:40 +0800

    add

---
+++ Assets/docmd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class docmd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void doPlay()
+	{
+		Debug.Log ("doPlay");
+		SceneManager.LoadScene ("demo");
+	}
+	public void doExit()
+	{
+		Debug.Log ("doExit");
+		Application.Quit ();
+	}
+
+}
